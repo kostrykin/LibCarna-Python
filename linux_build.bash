7da7935c98b1e3f78d4ca7b133f074526bf1d2e3
Leonid Kostrykin <void@evoid.de>
2025-04-17 12:16:47 +0200

    Clean up build script

---
+++ linux_build.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -ex
+
+# Create or update conda environment
+export ROOT=$(dirname "$0")
+if [ ! -d "$ROOT/.env" ]; then
+    conda env create -f "$ROOT/environment.yml" --prefix "$ROOT/.env"
+else
+    conda env update -f "$ROOT/environment.yml" --prefix "$ROOT/.env" --prune
+fi
+
+# Activate conda environment
+eval "$(conda shell.bash hook)"
+conda activate "$ROOT/.env"
+
+# Setup and check dependencies:
+export PYBIND11_PREFIX="$CONDA_PREFIX/share/cmake/pybind11"
+
+# Build wheel
+export CARNAPY_BUILD_TEST="OFF"
+cd "$ROOT"
+python setup.py bdist_wheel
