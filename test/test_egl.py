1b00a039a2ec2ba77ec4aacf05f3f45a3334a1e4
Leonid Kostrykin <void@evoid.de>
2025-04-27 11:07:04 +0200

    Add failing `EGLContext` test case

--- test/test_egl.py
+++ test/test_egl.py
@@ -1,3 +1,5 @@
+import gc
+
 import carna.egl
 
 import testsuite
@@ -5,6 +7,16 @@ import testsuite
 
 class EGLContext(testsuite.CarnaTestCase):
 
-    def test(self):
+    def test__init__(self):
         ctx = carna.egl.EGLContext()
         del ctx
+
+    def test__stack(self):
+        """
+        Test destruction of the active EGL context while another EGL context still exists (and will be activated by Carna).
+        """
+        ctx1 = carna.egl.EGLContext()
+        ctx2 = carna.egl.EGLContext()
+        del ctx2
+        gc.collect()
+        del ctx1
