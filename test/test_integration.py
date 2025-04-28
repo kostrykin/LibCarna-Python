10e7cd9693a0b72ff1c64e7a8b38f8b37e884236
Leonid Kostrykin <void@evoid.de>
2025-04-28 10:47:14 +0200

    Update docs

--- test/test_integration.py
+++ test/test_integration.py
@@ -116,7 +116,7 @@ class OpaqueRenderingStage(testsuite.CarnaRenderingTestCase):
         )
         camera = carna.camera(
             parent=root,
-            projection=r.frustum(fov=np.pi / 2, z_near=10, z_far=1000),
+            projection=r.frustum(fov=np.pi / 2, z_near=1, z_far=1e3),
             local_transform=carna.math.translation(0, 0, 250),
         )
 
