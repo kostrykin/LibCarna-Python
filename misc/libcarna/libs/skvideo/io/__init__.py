63295b7603c7cb8f30dd073e6cf2f9ad44690ddc
Leonid Kostrykin <void@evoid.de>
2025-05-15 23:03:47 +0200

    Clean up `libcarna.libs.skvideo`

--- misc/libcarna/libs/skvideo/io/__init__.py
+++ misc/libcarna/libs/skvideo/io/__init__.py
@@ -3,22 +3,8 @@
 """
 
 
-#from .avconv import *
-#from .avprobe import *
 from .ffmpeg import *
-#from .ffprobe import *
-#from .io import *
-#from .mprobe import *
 
 __all__ = [
-#    'vread',
-#    'vreader',
-#    'vwrite',
-#    'mprobe',
-#    'ffprobe',
-#    'avprobe',
-#    'FFmpegReader',
     'FFmpegWriter',
-#    'LibAVReader',
-#    'LibAVWriter'
 ]
